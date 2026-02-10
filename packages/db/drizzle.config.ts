cf447e1be52c70e236afec93e2f50d386f933672
Snipyer <snipyergaming@gmail.com>
2026-02-10 14:22:16 +0100

    switch to using bun-sqlite

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
+import { resolve } from "path";
 
 dotenv.config({
 	path: "../../apps/server/.env",
 });
 
+const dbPath = resolve("../../apps/server", process.env.DATABASE_URL || "local.db");
+
 export default defineConfig({
 	schema: "./src/schema",
 	out: "./src/migrations",
-	dialect: "turso",
+	dialect: "sqlite",
 	dbCredentials: {
-		url: process.env.DATABASE_URL || "",
+		url: dbPath,
 	},
 });
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,11 @@
-import { createClient } from "@libsql/client";
+import { Database } from "bun:sqlite";
 import { env } from "@offline-sqlite/env/server";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { resolve } from "path";
 
 import * as schema from "./schema";
 
-const client = createClient({
-	url: env.DATABASE_URL,
-});
+const dbPath = resolve(process.cwd(), env.DATABASE_URL);
+const client = new Database(dbPath);
 
 export const db = drizzle({ client, schema });
