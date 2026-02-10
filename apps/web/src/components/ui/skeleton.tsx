a7b4e75dd20dd75feedd2a61d7d8a0ad9d484e76
Snipyer <snipyergaming@gmail.com>
2026-02-10 14:02:24 +0100

    init

---
+++ apps/web/src/components/ui/skeleton.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils";
+
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+	return (
+		<div
+			data-slot="skeleton"
+			className={cn("bg-muted animate-pulse rounded-none", className)}
+			{...props}
+		/>
+	);
+}
+
+export { Skeleton };
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@offline-sqlite/db";
+import * as schema from "@offline-sqlite/db/schema/auth";
+import { env } from "@offline-sqlite/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "sqlite",
+
+		schema: schema,
+	}),
+	trustedOrigins: [env.CORS_ORIGIN],
+	emailAndPassword: {
+		enabled: true,
+	},
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
+	plugins: [],
+});
